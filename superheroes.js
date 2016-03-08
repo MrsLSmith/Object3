66d18f01563fe8bf8325ebfc29f1d7d0af23afe9
Calvin <cpowell@ccsuvt.org>
2016-03-08 10:16:01 -0500

    hey

--- superheroes.js
+++ superheroes.js
@@ -1,32 +1,27 @@
 var superheroes = [{
-  Name: "Incredible Hulk",
-  Color: "Green",
-  Superpower: "Strength"
+  Name: "Nick Pettit",
+  Color: "White",
+  Superpower: "Coding"
 }, {
-  Name: "Superman",
-  Color: "Blue",
-  Superpower: "Flying"
+  Name: "Batman",
+  Color: "Black",
+  Superpower: "Money"
 }, {
-  Name: "Hawkeye",
-  Color: "Purple",
-  Superpower: "Archery"
+  Name: "Deadpool",
+  Color: "Red",
+  Superpower: "Comedy"
 }];
 
 
 
-function listHeadings() {
-    var table = (Object.keys(superheroes[0]));
-  for(var prop in table) {
-    document.getElementById("output").innerHTML += "<p>" + table[prop] + "</p>";
-  }
-}
 
-function toggle() {
-	var el = document.getElementById("output");
-	if ( el.style.display != 'none' ) {
-		el.style.display = 'none';
-	}
-	else {
-		el.style.display = '';
-	}
+function list() {
+ 
+var letter = ""
+for (var i = 0; i < 3; i += 1) {
+  letter += "<p>";
+  letter += superheroes[i].Name;
+  letter += '</p>';
+}  
+  document.getElementById("div").innerHTML = letter
 }
