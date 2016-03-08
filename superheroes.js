7ea57bf87193701fc3bf73ba8ab3af5102d315d0
SMendes <semendes@ccsuvt.org>
2016-03-08 10:03:35 -0500

    Sergio's part 2

--- superheroes.js
+++ superheroes.js
@@ -11,3 +11,22 @@ var superheroes = [{
   Color: "Purple",
   Superpower: "Archery"
 }];
+
+
+
+function listHeadings() {
+    var table = (Object.keys(superheroes[0]));
+  for(var prop in table) {
+    document.getElementById("output").innerHTML += "<p>" + table[prop] + "</p>";
+  }
+}
+
+function toggle() {
+	var el = document.getElementById("output");
+	if ( el.style.display != 'none' ) {
+		el.style.display = 'none';
+	}
+	else {
+		el.style.display = '';
+	}
+}
