1b49d435fe7261120185423b475fc8dfbd5348e4
DanielM <dmeskill@ccsuvt.org>
2016-03-08 10:04:11 -0500

    table added

--- superheroes.js
+++ superheroes.js
@@ -11,3 +11,28 @@ var superheroes = [{
   Color: "Red",
   Superpower: "Comedy"
 }];
+
+function toggle() {
+  var el = document.getElementById("Table");
+  if (el.style.display != 'none') {
+    el.style.display = 'none';
+  } else {
+    el.style.display = '';
+  }
+};
+
+var HeroesTable1 = "";
+var HeroesTable2 = "";
+var HeroesTable3 = "";
+var Heroes = "";
+var HeroStuff = "";
+
+for (var i = 0; i < superheroes.length; i += 1) {
+  Heroes = superheroes[i];
+  HeroesTable1 += "<p class='rows'>" + Heroes.Name + "</p>";
+  HeroesTable2 += "<p class='rows'>" + Heroes.Color + "</p>";
+  HeroesTable3 += "<p class='rows'>" + Heroes.Superpower + "</p>";
+  document.getElementById("TableColumnOne").innerHTML = HeroesTable1;
+  document.getElementById("TableColumnTwo").innerHTML = HeroesTable2;
+  document.getElementById("TableColumnThree").innerHTML = HeroesTable3;
+}
